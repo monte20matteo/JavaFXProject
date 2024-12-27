4c1d38fa85ff9e67b17c73dad742c436a988b124
Matteo Monteventi <matteo.monteventi@studio.unibo.it>
2024-12-27 18:48:47 +0100

    update

--- src/Prova.java
+++
@@ -1,8 +0,0 @@
-// creata con Ctrl+i --> GitHub CoPilot
-public class Prova {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
-        }
-    }
-}
