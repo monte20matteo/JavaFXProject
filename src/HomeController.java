4743a294159c99d1f1913a520cf3d509875af61d
Matteo Monteventi <matteo.monteventi@studio.unibo.it>
2025-01-04 22:12:48 +0100

    parziale

--- src/HomeController.java
+++ src/HomeController.java
@@ -11,7 +11,7 @@ import javafx.scene.layout.BorderPane;
 public class HomeController {
 
     @FXML
-    private Label target; // Dichiarazione del campo target, collegato all'elemento Label nel file FXML
+    private Label target1; // Dichiarazione del campo target1, collegato all'elemento Label nel file FXML
 
     @FXML
     void loginClicked(ActionEvent event) {
@@ -19,7 +19,7 @@ public class HomeController {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/Login/Login.fxml"));
             Parent login = loader.load(); 
             Scene loginScene = new Scene(login); 
-            Stage loginWindow = (Stage) target.getScene().getWindow();
+            Stage loginWindow = (Stage) target1.getScene().getWindow();
 
             loginWindow.setScene(loginScene); // Imposta la scena appena creata nella finestra di login
             loginWindow.show();
@@ -31,7 +31,23 @@ public class HomeController {
 
     @FXML
     void registrazioneClicked(ActionEvent event) {
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Registrazione/Registrazione.fxml"));
+            // Carica il file FXML di Register.fxml utilizzando il caricatore FXMLLoader
 
+            Parent register = loader.load(); // Carica la radice del file FXML (AnchorPane) per la finestra di registrazione
+
+            Scene regScene = new Scene(register); // Crea una nuova scena utilizzando la radice caricata
+
+            Stage regWindow = (Stage) target1.getScene().getWindow();
+            // Ottiene il riferimento alla finestra corrente (della Home) per impostare la nuova scena
+
+            regWindow.setScene(regScene); // Imposta la scena appena creata nella finestra di registrazione
+            regWindow.show(); // Mostra la finestra di registrazione
+        } catch(Exception e){
+            System.out.println("Verificato un errore nel caricamento della finestra di registrazione: --> "+e.getMessage());
+            // Gestione dell'eccezione nel caso in cui il caricamento della finestra di registrazione fallisca
+        }
     }
 
 }
--- src/Registrazione/RegistrazioneController.java
+++ src/Registrazione/RegistrazioneController.java
@@ -1,4 +1,4 @@
-package Login;
+package Registrazione;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -9,7 +9,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class LoginController {
+public class RegistrazioneController {
 
     @FXML
     private Label target; // Dichiarazione del campo target, collegato all'elemento Label nel file FXML
